46be41c469ec6c1ce21abb92637be5a8803bcb91
salman486 <72623659+salman486@users.noreply.github.com>
2021-01-12 13:54:05 +0000

    Schedule added

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Header from "./components/Header";
 import Popup from "./components/Popup";
+import Schedule from "./components/Schedule";
 // import { v4 as uuidv4 } from "uuid";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div>
       <Popup />
       <Header />
+      <Schedule />
     </div>
   );
 }
---
+++ app/src/components/Schedule.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import "./style.css";
+import "./Schedule.css";
+
+class Schedule extends Component {
+  render() {
+    // const name = {border-top:'1px solid black'};
+    return (
+      <div className="Schedule">
+        <div className="Schedule-teams-container">
+          <p className="Schedule-teams">Remaining Teams: 128</p>
+        </div>
+
+        <section className="Schedule-area"></section>
+
+        <button className="Schedule-submit-btn btn">COPY YOUR SCHEDULE</button>
+        <button className="Schedule-reset-btn btn disabled" disabled>
+          RESET SCHEDULE
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Schedule;
