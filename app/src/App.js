3e3c08130388509d2f3f8b367664e22376d7275a
salman486 <72623659+salman486@users.noreply.github.com>
2021-01-12 12:39:05 +0000

    Popup Added

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/Header";
+import Popup from "./components/Popup";
 // import { v4 as uuidv4 } from "uuid";
 
 function App() {
   return (
     <div>
+      <Popup />
       <Header />
     </div>
   );
---
+++ app/src/components/Popup.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Popup.css";
+import "./style.css";
+
+const Popup = () => {
+  return (
+    <section className="hidden">
+      <div className="Popup"></div>
+      <div className="Popup-content">
+        <div>
+          <label htmlFor="teams" className="Popup-teams--label">
+            Entry Number of Teams:{" "}
+          </label>
+          <input type="number" className="Popup-teams--input" id="teams" />
+        </div>
+        <button className="btn Popup-btn">Submit</button>
+      </div>
+    </section>
+  );
+};
+
+export default Popup;
