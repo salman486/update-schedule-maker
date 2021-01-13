7c22c7bd228afcd448a3310606950d72beefbfc0
salman486 <72623659+salman486@users.noreply.github.com>
2021-01-13 13:36:09 +0000

    need to solve yet

--- app/src/components/Schedule.js
+++ app/src/components/Schedule.js
@@ -1,19 +1,36 @@
-import React, { useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import Teams from "./Teams";
 import "./style.css";
 import "./Schedule.css";
 import { TeamsContext } from "../contexts/TeamsContext";
+import { v4 as uuidv4 } from "uuid";
 
 function Schedule(props) {
   const totalTeams = useContext(TeamsContext);
+  const [teams, setTeams] = useState([1]);
+
+  useEffect(() => {
+    setTeams([{ teamId: uuidv4().slice(0, 8), teamName: "", teamValue: "" }]);
+    console.log(totalTeams);
+  }, [totalTeams]);
+
+  const addTeams = (team) => {
+    setTeams((prevTeams) => {
+      console.log(prevTeams);
+      return [...prevTeams, { teamId: uuidv4().slice(0, 8), ...team }];
+    });
+  };
+
   return (
     <div className={`Schedule ${props.showSchedule ? "" : "hidden"}`}>
       <div className="Schedule-teams-container">
-        <p className="Schedule-teams">Remaining Teams: {totalTeams}</p>
+        <p className="Schedule-teams">Remaining Teams: </p>
       </div>
 
       <section className="Schedule-area">
-        <Teams />
+        {teams.map((_) => (
+          <Teams addTeams={addTeams} key={uuidv4()} />
+        ))}
       </section>
 
       <button className="Schedule-submit-btn btn disabled" disabled>
--- app/src/components/Teams.js
+++ app/src/components/Teams.js
@@ -1,34 +1,11 @@
-// import React, { useContext } from "react";
+import React, { useState } from "react";
 // import { TeamsContext } from "../contexts/TeamsContext";
 import "./style.css";
 import "./Teams.css";
 
-import { v4 as uuidv4 } from "uuid";
-
-let counter = 0;
-let currentTeams = 0;
-let teamsData = [];
-
-function Teams() {
-  function getTeamData() {
-    const teamNameEl = document.querySelector(`#tname-${counter}`);
-    const teamValueEl = document.querySelector(`#tvalue-${counter}`);
-
-    const teamName = teamNameEl.value.trim();
-    const teamValue = +teamValueEl.value;
-
-    if (teamName.length === 0 || teamValue <= 0 || !Number.isFinite(teamValue))
-      return;
-    teamsData.push({ teamId: uuidv4().slice(0, 8), teamName, teamValue });
-    currentTeams += teamValue;
-    counter++;
-
-    const btnEl = document.querySelector(".Teams-btn");
-    btnEl.classList.add("Teams-btn--disabled");
-    teamNameEl.disabled = true;
-    teamValueEl.disabled = true;
-    btnEl.disabled = true;
-  }
+function Teams(props) {
+  const [teamName, setTeamName] = useState("");
+  const [teamValue, setTeamValue] = useState("");
 
   return (
     <div className="Teams">
@@ -36,17 +13,24 @@ function Teams() {
       <input
         type="text"
         className="Teams-input Teams-input--text"
-        id={`tname-${counter}`}
+        onChange={(e) => setTeamName(e.target.value)}
+        value={teamName}
       />
 
       <label className="Teams-label">Enter teams: </label>
       <input
         type="number"
         className="Teams-input Teams-input--number"
-        id={`tvalue-${counter}`}
+        onChange={(e) => setTeamValue(e.target.value)}
+        value={teamValue}
       />
 
-      <button className="btn Teams-btn" onClick={getTeamData}>
+      <button
+        className="btn Teams-btn"
+        onClick={() => {
+          props.addTeams([teamName, teamValue]);
+        }}
+      >
         +
       </button>
     </div>
